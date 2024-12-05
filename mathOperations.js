5841f55f8d98ef079354dc12e6f9c0c6006e6dcf
SatenikGabrielyan <gabrielyansatenik1@gmail.com>
2024-12-06 00:10:11 +0400

    add error handling

--- mathOperations.js
+++ mathOperations.js
@@ -1,20 +1,34 @@
 function add(a,b){
+    if(typeof a !== "number" || typeof b !== "number"){
+        throw new Error("Parameter must be number")
+    }
     return a + b
 }
 
 function subtract(a, b){
+    if(typeof a !== "number" || typeof b !== "number"){
+        throw new Error("Parameter must be number")
+    }
     return a - b
 }
 
 function multiply(a, b){
+    if(typeof a !== "number" || typeof b !== "number"){
+        throw new Error("Parameter must be number")
+    }
     return a * b
 }
 
 function divide(a, b){
+    if(typeof a !== "number" || typeof b !== "number"){
+        throw new Error("Parameter must be number")
+    }
     if(b == 0){
         throw new Error("Cannot devide by zero")
     }
     return a / b
 }
+
+console.log(add("fgd","gfkj"))
 
 module.exports = {add, subtract, multiply, divide}
